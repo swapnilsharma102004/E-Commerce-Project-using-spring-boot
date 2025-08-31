4c4d974b3599a3df0f08d6a87897b94cf1f7705b
swapnilsharma102004 <swapnilsharma1369@gmail.com>
2025-08-31 17:39:54 +0530

    commit

--- src/main/java/com/swapnil/Ecommerce_Project/controller/ProductController.java
+++ src/main/java/com/swapnil/Ecommerce_Project/controller/ProductController.java
@@ -91,4 +91,12 @@ public class ProductController {
             return new ResponseEntity<>("Product not Found", HttpStatus.NOT_FOUND);
         }
     }
+
+    @GetMapping("/products/search")
+    public ResponseEntity<List<Product>> searchProduct(@RequestParam String keyword){
+        System.out.printf("Searching with" + keyword);
+        List<Product> products= service.searchProducts(keyword);
+        return new ResponseEntity<>(products,HttpStatus.OK);
+
+    }
 }
